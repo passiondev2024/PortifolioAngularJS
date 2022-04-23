881eb5229cac955cbbc429c33ab3f9ef43c41dd1
kanandev2024 <kananramazandev@gmail.com>
2022-04-23 20:42:45 -0300

    first commit

--- ipsm-go-portal-developer/src/components/include/Footer.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  const date = new Date();
-  let full_year = date.getFullYear();
-</script>
-
--- ipsm-go-portal-developer/src/components/include/Store.js
+++
@@ -1,9 +0,0 @@
-import {
-    writable
-} from "svelte/store";
-import {
-    afterUpdate
-} from "svelte";
-
-let toDark;
-export const dark = writable(toDark = "dark");
