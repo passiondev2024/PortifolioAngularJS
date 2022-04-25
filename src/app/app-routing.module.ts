1486c4fee43317d54e319e31679dd2b5a4d52d58
kanandev2024 <kananramazandev@gmail.com>
2022-04-24 22:18:59 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {AppComponent} from './app.component';
 
 const routes: Routes = [
-  {path: "", component: AppComponent},
+  
 ];
 
 @NgModule({
