3df559a82d2862972a39188c875c43d80ba0de26
kanandev2024 <kananramazandev@gmail.com>
2022-04-27 12:22:28 -0300

    alteração de bug

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
-  telefone: string = "../../assets/telefone.png";
+  telefone: string = "../../assets/telefone2.png";
   images: string[] = [ "1.jpg", "3.jpg", "5.jpg", "netflix.png", "2.jpg",  "4.jpg",  "6.jpg"];
   widthSlider: string = `calc(220px * ${this.images.length})`;
   ngOnInit(): void {
