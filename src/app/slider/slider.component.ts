d8584091b6a55c7c28bd723999c8bab89200ef78
kanandev2024 <kananramazandev@gmail.com>
2024-03-21 10:54:41 -0300

    putting more images on slider

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
   telefone: string = "../../assets/telefone2.png";
-  images: string[] = [ "1.jpg", "3.jpg", "5.jpg", "netflix.png", "2.jpg",  "4.jpg",  "6.jpg"];
+  images: string[] = [ "1.jpg", "3.jpg", "5.jpg",  "2.jpg",  "4.jpg",  "6.jpg",  "7.jpeg", 
+     "8.jpeg",  "9.jpeg",  "10.jpeg",  "11.jpeg",  "12.jpeg",  "13.jpeg"];
   widthSlider: string = `calc(220px * ${this.images.length})`;
   ngOnInit(): void {
   
